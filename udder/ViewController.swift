a5c1573cd96cc5a7a531e4dbad062ecab284eeb9
BianfuruPro <BianfuruPro@MacBook-Pro.local>
2018-11-20 15:02:20 -0500

    Initial Commit

---
+++ udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udder
+//
+//  Created by BianfuruPro on 11/20/18.
+//  Copyright © 2018 bianfuru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
